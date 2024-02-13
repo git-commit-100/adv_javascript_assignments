56eecf2603c7f4aecf811c43ac8c3573bc7a6dab
Java Monk <javamonk.coder@gmail.com>
2024-02-12 21:48:00 -0500

    implementing task list

--- Part1-task_list/task_list/task_list.js
+++ Part1-task_list/task_list/task_list.js
@@ -1,30 +1,45 @@
 "use strict";
 
-$(document).ready( () => {
-    const tasks = [];
+$(document).ready(() => {
+  const tasks = [];
 
-    $("#add_task").click( () => {   
-        const textbox = $("#task");
-        const task = textbox.val();
-        if (task === "") {
-            alert("Please enter a task.");
-            textbox.focus();
+  $("#add_task").click(() => {
+    const textbox = $("#task");
+    const task = textbox.val();
+    if (task === "") {
+      alert("Please enter a task.");
+      textbox.focus();
+    } else {
+      /* 
+      splitting task string to different tasks(items), then
+      getting rid of whitespace between , and task
+      */
+      let sanitizedTasksArr = task.split(",").map((task) => task.trim());
+
+      // add only VALID task to array
+      for (let item of sanitizedTasksArr) {
+        if (item !== "") {
+          tasks.push(item);
         } else {
-            // add task to array
-            tasks.push(task);
-
-            // clear task text box and re-display tasks
-            textbox.val("");
-            $("#task_list").val(tasks.join("\n"));
-            textbox.focus();
+          alert("Oops ! Some tasks are empty. Cannot insert empty tasks :(");
+          textbox.focus();
+          return;
         }
-    });
-    
-    $("#clear_tasks").click( () => {
-        tasks.length = 0;
-        $("#task_list").val("");
-        $("#task").focus();
-    }); 
-    
+      }
+
+      // clear task text box and re-display tasks
+      textbox.val("");
+      $("#task_list").val(tasks.join("\n"));
+      textbox.focus();
+    }
+  });
+
+  $("#clear_tasks").click(() => {
+    // clearing up array
+    tasks.length = 0;
+    $("#task_list").val("");
     $("#task").focus();
+  });
+
+  $("#task").focus();
 });
