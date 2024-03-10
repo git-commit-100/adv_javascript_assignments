9dce1e311b6b49baaa7bc49168b7aa196497bbb5
Java Monk <javamonk.coder@gmail.com>
2024-03-10 19:39:37 -0400

    completed part2

--- Part2-change_class/change_class/calculate.js
+++ Part2-change_class/change_class/calculate.js
@@ -1,42 +1,28 @@
 "use strict";
 
-$(document).ready( () => {
-    $("#calculate").click( () => {
-        // get the number of cents from the user
-        let cents = Math.floor( parseInt( $("#cents").val() ) );
+import Coins from "./library_coin.js";
 
-        if (isNaN(cents) || cents < 0 || cents > 99) {
-            alert("Please enter a valid number between 0 and 99");
-        } else {
-            // calculate the number of quarters
-            let quarters = cents / 25;  // get number of quarters
-            quarters = Math.floor(quarters);
-            cents = cents % 25;         // assign the remainder to the cents variable
+$(document).ready(() => {
+  $("#calculate").click(() => {
+    // get the number of cents from the user
+    let cents = Math.floor(parseInt($("#cents").val()));
 
-            // calculate the number of dimes
-            let dimes = cents / 10;     // get number of dimes
-            dimes = Math.floor(dimes);
-            cents = cents % 10;         // assign the remainder to the cents variable
+    if (isNaN(cents) || cents < 0 || cents > 99) {
+      alert("Please enter a valid number between 0 and 99");
+    } else {
+      let c = new Coins();
+      c.makeChange(cents);
+      // display the results of the calculations
+      $("#quarters").val(c.quarters);
+      $("#dimes").val(c.dimes);
+      $("#nickels").val(c.nickels);
+      $("#pennies").val(c.pennies);
 
-            // calculate the number of nickels
-            let nickels = cents / 5;
-            nickels = Math.floor(nickels);
+      // set focus on cents text box
+      $("#cents").focus();
+    }
+  }); // end click() method
 
-            // calculate the number of nickels and pennies
-            let pennies = cents % 5;
-
-            // display the results of the calculations
-            $("#quarters").val( quarters );
-            $("#dimes").val( dimes );
-            $("#nickels").val( nickels );
-            $("#pennies").val( pennies );
-            
-            // set focus on cents text box
-            $("#cents").focus();
-        }
-    }); // end click() method
-    
-    // set focus on cents text box on initial load
-    $("#cents").focus();
-            
+  // set focus on cents text box on initial load
+  $("#cents").focus();
 }); // end ready() method
