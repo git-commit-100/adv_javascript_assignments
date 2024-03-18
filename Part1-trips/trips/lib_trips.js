25726a0f011715a85b2e310972d36cc1615c50f8
Java Monk <javamonk.coder@gmail.com>
2024-03-17 21:55:37 -0400

    complete part1

--- Part1-trips/trips/lib_trips.js
+++ Part1-trips/trips/lib_trips.js
@@ -1,60 +1,69 @@
 "use strict";
 
-class Trip {
-    constructor(destination, km, litres) {
-        this.destination = destination;
-        this.km = parseFloat(km);
-        this.litres = parseFloat(litres);
-    }
+export class Trip {
+  constructor(destination, km, litres) {
+    this.destination = destination;
+    this.km = parseFloat(km);
+    this.litres = parseFloat(litres);
+  }
 
-    get isValid() {              // a read-only property
-        if (this.destination == "" || isNaN(this.km) || isNaN(this.litres)) {
-            return false;
-        } else if (this.km <= 0 || this.litres <= 0){
-            return false;
-        } else {
-            return true;
-        }
+  get isValid() {
+    // a read-only property
+    if (this.destination == "" || isNaN(this.km) || isNaN(this.litres)) {
+      return false;
+    } else if (this.km <= 0 || this.litres <= 0) {
+      return false;
+    } else {
+      return true;
     }
+  }
 
-    get kml() {                  // a read-only property
-        return this.km / this.litres;
-    }
+  get kml() {
+    // a read-only property
+    return this.km / this.litres;
+  }
 
-    toString() {
-        const kml = this.kml.toFixed(1);
-        return `${this.destination}: Kilometers - ${this.km}; KML - ${kml}`;
-    }
+  toString() {
+    const kml = this.kml.toFixed(1);
+    return `${this.destination}: Kilometers - ${this.km}; KML - ${kml}`;
+  }
 }
 
-class Trips {
-    constructor() {
-        this._trips = [];
-    }
+// using closre Trips
+export const Trips = (function () {
+  // private array; due to closure
+  let tripsArr = [];
 
-    push(trip) {
-        // only allow Trip objects to be added to array
-        if (trip instanceof Trip) {
-            this._trips.push(trip);
-        }
+  function push(trip) {
+    // only allow Trip objects to be added to array
+    if (trip instanceof Trip) {
+      tripsArr.push(trip);
     }
+  }
 
-    get totalKml() {             // a read-only property
-        let totalKm = 0;        
-        let totalLitres = 0;        
-        for (let trip of this._trips) {
-            totalKm += trip.km;
-            totalLitres += trip.litres;
-        }
-        return totalKm / totalLitres;
+  function totalKml() {
+    // a read-only property
+    let totalKm = 0;
+    let totalLitres = 0;
+    for (let trip of tripsArr) {
+      totalKm += trip.km;
+      totalLitres += trip.litres;
     }
+    return totalKm / totalLitres;
+  }
 
-    toString() {
-        let str = "";
-        for (let trip of this._trips) {
-            str += trip.toString() + "\n";
-        }
-        str += "\nCumulative KML: " + this.totalKml.toFixed(1);
-        return str;
+  function toString() {
+    let str = "";
+    for (let trip of tripsArr) {
+      str += trip.toString() + "\n";
     }
-}
+    str += "\nCumulative KML: " + totalKml().toFixed(1);
+    return str;
+  }
+
+  return {
+    push,
+    totalKml,
+    toString,
+  };
+})();
--- Part1-trips/trips/trips.js
+++ Part1-trips/trips/trips.js
@@ -1,28 +1,31 @@
 "use strict";
+import { Trip, Trips } from "./lib_trips.js";
 
-$(document).ready( () => {
-    const trips = new Trips();
+$(document).ready(() => {
+  $("#add_trip").click(() => {
+    const trip = new Trip(
+      $("#destination").val(),
+      $("#km").val(),
+      $("#litres").val()
+    );
 
-    $("#add_trip").click( () => {
-        const trip = new Trip( 
-          $("#destination").val(), $("#km").val(), $("#litres").val() );
+    if (trip.isValid) {
+      Trips.push(trip);
+      $("#trip_list").val(Trips.toString());
 
-        if (trip.isValid) {
-            trips.push(trip); 
-            $("#trip_list").val(trips.toString());
+      $("#destination").val("");
+      $("#km").val("");
+      $("#litres").val("");
 
-            $("#destination").val("");
-            $("#km").val("");
-            $("#litres").val("");
+      $("#destination").focus();
+    } else {
+      alert(
+        "Please complete all fields.\nKilometers and litres " +
+          "must be numeric and greater than zero."
+      );
+      $("#destination").select();
+    }
+  });
 
-            $("#destination").focus();
-        } 
-        else {
-            alert("Please complete all fields.\nKilometers and litres " 
-                + "must be numeric and greater than zero.");
-            $("#destination").select();
-        }
-    });
-    
-    $("#destination").focus();
+  $("#destination").focus();
 });
